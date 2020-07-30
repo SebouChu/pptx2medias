4a6f5377e77af3ea99770ad115d31b3e57e26625
Sébastien Gaya <sebastien.gaya@gmail.com>
2020-07-30 09:57:36 +0200

    First version

---
+++ lib/pptx2medias.rb
@@ -0,0 +1,35 @@
+module Pptx2Medias
+  VERSION = '0.0.1'
+
+  def self.extract(file_path)
+    file_type = `file -b "#{file_path}"`
+    file_ext = File.extname(file_path)
+
+    puts "- Input file: #{file_path}"
+    puts "- File type: #{file_type}"
+
+    unless file_type.include?('PowerPoint') && file_ext == '.pptx'
+      puts 'Input file is not a valid PPTX. Exiting...'
+      return
+    end
+
+    puts 'Input file is a PowerPoint presentation. Processing...'
+
+    dir = File.dirname(file_path)
+    basename = File.basename(file_path, file_ext)
+
+    system("cp \"#{file_path}\" \"#{dir}/archive.zip\"")
+    system("unzip \"#{dir}/archive.zip\" ppt/media/* -d #{dir}")
+    system("mv \"#{dir}/ppt/media\" \"#{dir}/#{basename}_media\"")
+
+    medias_count = `ls -1 \"#{dir}/#{basename}_media\" | wc -l`.strip.to_i
+    videos_count = `ls -1 \"#{dir}/#{basename}_media\" | grep .mp4 | wc -l`.strip.to_i
+
+    puts "File has been processed. Found #{medias_count} medias (including #{videos_count} videos). Cleaning..."
+
+    system("rmdir \"#{dir}/ppt\"")
+    system("rm \"#{dir}/archive.zip\"")
+
+    puts "Done!"
+  end
+end
---
+++ pptx2medias.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name          = 'pptx2medias'
+  s.version       = '0.0.1'
+  s.platform      = Gem::Platform::RUBY
+  s.summary       = 'Get medias from your PowerPoint presentation!'
+  s.description   = "Extracts media files (images, videos, sounds) from a PPTX file."
+  s.authors       = ['Sébastien Gaya']
+  s.email         = ['sebastien.gaya@gmail.com']
+  s.homepage      = 'http://rubygems.org/gems/pptx2medias'
+  s.license       = 'MIT'
+  s.files         = Dir.glob("{lib,bin}/**/*")
+  s.require_path  = 'lib'
+  s.executables = ['pptx2medias']
+end
